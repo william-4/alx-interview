38e33be6d184d38aff7fc7dea8824b4e9df1a46b
chan.gretta <93374467+william-4@users.noreply.github.com>
2023-02-15 20:14:55 +0300

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,22 +9,15 @@ def rotate_2d_matrix(matrix):
     Function that rotates a 2d n * n matrix 90 degrees clockwise
     """
     # First Transpose the matrix
-    i = 0
-    while i < len(matrix):
-        j = i
-        while j < len(matrix[0]):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i):
             temp = matrix[i][j]
             matrix[i][j] = matrix[j][i]
             matrix[j][i] = temp
-            j += 1
-        i += 1
-    # Second Reverse each row
-    for row in matrix:
-        i = 0  # start index pointer
-        j = len(row) - 1  # last index pointer
-        while i < j//2:
-            temp = row[i]
-            row[i] = row[j]
-            row[j] = temp
-            i += 1
-            j -= 1
+    # Second Reverse the matrix
+    for i in range(n):
+        for j in range(int(n / 2)):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][n-1-j]
+            matrix[i][n-1-j] = 
