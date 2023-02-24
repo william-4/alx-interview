07c70d07bb613b4eed738478ba6f4a46c1eda457
WILLIAM MWANGI KARIUKI <T35518873@kra.gov>
2023-02-24 16:55:05 +0300

    task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+def makeChange(coins, total):
+    """
+    Determing fewest number of coins needed to meet a given amount total
+    Args:
+        coins (list of ints): list of coins of different values
+        total (int): total value to be met
+    Return:
+        Number of coins or 
+        -1 if total cannot be met by any number of coins
+        0 if total <= 0
+    """
+    count = 0
+
+    if total <= 0:
+        return (0)
+    if coins == [] or coins is None:
+        return (-1)
+
+    coins.sort(reverse=True)
+
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            count += 1
+    if total > 0:
+        return (-1)
+    return (count)
